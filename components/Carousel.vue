9ba9f4ed5159cdd573118cce8032e37289129425
fjp712 <45873259+fjp712@users.noreply.github.com>
2020-10-22 11:39:31 +0800

    Add files via upload

---
+++ components/Carousel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="carousel">
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            setTimeout()
+        },
+        beforeDestroy() {
+            clearTimeout()
+        },
+        name: "Carousel"
+    }
+</script>
+
+<style scoped>
+    .carousel{
+        height: 300px;
+        background-color: #f5ede5;
+    }
+</style>
---
+++ components/main.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="main">
+        <div class="nav">
+            <div v-for="item in navitems" :key="item" class="navitem">
+                <div class="nav_word">{{item}}</div>
+            </div>
+            <input type="search">
+            <div class="login"><div class="nav_word">登录|注册</div></div>
+        </div>
+        <div class="Carousel">
+            <Carousel/>
+        </div>
+    </div>
+</template>
+<script>
+    import Carousel from "./Carousel";
+    import {mapState} from "vuex"
+    export default {
+        data:function(){
+            return{
+                navitems:["主页","商品列表","文章列表"]
+            }
+
+        },
+        computed: {
+            ...mapState(["count"]),
+        },
+        components:{
+            Carousel
+        }
+    }
+</script>
+<style scoped>
+    .main{
+        display: flex;
+        justify-content: center;
+        height: 800px;
+        width: 100%;
+        flex-wrap: wrap;
+    }
+    .nav{
+        display: flex;
+        width: 80%;
+        height: 60px;
+    }
+    .navitem{
+        flex: 0 1 auto;
+        height: 60px;
+        width: 15%;
+    }
+    .navitem:hover{
+        background-color: #f5ede5;
+    }
+    .nav_word{
+        padding-top: 10px;
+    }
+    .login{
+        align-self: flex-end;
+        height: 60px;
+        width: 20%;
+    }
+    input[type="search"]{
+        outline: none;
+        border-radius: 15px;
+        height: 30px;
+        margin-top:10px ;
+    }
+    .Carousel{
+        width:80%;
+        margin-top: -300px;
+    }
+
+
+</style>
